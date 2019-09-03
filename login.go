f7f81f9a1abe4ff02eb766bdbeeecbb53ca14b3f
hugues <ghugues@netc.fr>
2019-09-03 18:51:15 +0200

    add delay to exit in fail

--- login.go
+++ login.go
@@ -8,9 +8,14 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 func main() {
+	time.AfterFunc(3 * 1000*1000*1000,func() {
+		fmt.Fprintln(os.Stderr, "Temps écoulé (2s)")
+		os.Exit(1)
+	})
 	rep, err := http.PostForm("https://wlc1.reseau.uvsq.fr/login.html",
 		url.Values{
 			"username": {"LOGIN"},
